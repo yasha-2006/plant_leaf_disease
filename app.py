72d595b11cf248fb2653ed4e119577ed10858591
yasha-2006 <yashashreem2006@gmail.com>
2026-02-16 19:29:52 +0530

    Add plant disease detection system with Streamlit
    Implement a plant disease detection system using Streamlit and TensorFlow. Includes model downloading, image processing, and prediction functionalities.

---
+++ app.py
@@ -0,0 +1,59 @@
+import streamlit as st
+import tensorflow as tf
+import numpy as np
+import gdown
+import os
+
+file_id="1VpmAN2DJExQ3wGblX4TLhC2Os9CXjcOC"
+url="https://drive.google.com/file/d/1VpmAN2DJExQ3wGblX4TLhC2Os9CXjcOC/view?usp=drive_link"
+model_path="trained_plant_disease_model.keras"
+
+
+if not os.path.exists(model_path):
+    st.warning("Downloading model from Google Drive...")
+    gdown.download(id=file_id, output=model_path, quiet=False,fuzzy=True)
+
+def load_model():
+    return tf.keras.models.load_model(model_path)
+
+model = load_model()
+
+def model_prediction(test_image):
+    image = tf.keras.preprocessing.image.load_img(test_image,target_size=(128,128))
+    input_arr = tf.keras.preprocessing.image.img_to_array(image)
+    input_arr = np.array([input_arr]) #convert single image to batch
+    predictions = model.predict(input_arr)
+    return np.argmax(predictions) #return index of max element
+
+#Sidebar
+st.sidebar.title("Plant Disease Detection System for Sustainable Agriculture")
+app_mode = st.sidebar.selectbox("Select Page",["HOME","DISEASE RECOGNITION"])
+#app_mode = st.sidebar.selectbox("Select Page",["Home"," ","Disease Recognition"])
+
+# import Image from pillow to open images
+from PIL import Image
+img = Image.open("Disease.png")
+
+# display image using streamlit
+# width is used to set the width of an image
+st.image(img)
+
+#Main Page
+if(app_mode=="HOME"):
+    st.markdown("<h1 style='text-align: center;'>Plant Disease Detection System for Sustainable Agriculture", unsafe_allow_html=True)
+    
+#Prediction Page
+elif(app_mode=="DISEASE RECOGNITION"):
+    st.header("Plant Disease Detection System for Sustainable Agriculture")
+    test_image = st.file_uploader("Choose an Image:")
+    if(st.button("Show Image")):
+        st.image(test_image,width=4,use_column_width=True)
+    #Predict button
+    if(st.button("Predict")):
+        st.snow()
+        st.write("Our Prediction")
+        result_index = model_prediction(test_image)
+        #Reading Labels
+        class_name = ['Early_Blight', 'Healthy', 'Late_Blight']
+        st.success("Model is Predicting it's a {}".format(class_name[result_index]))
+
